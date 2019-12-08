b998300ca8d5f4f8d7f03020570932a0e19faa26
GabrielGhimici <gabriel.ghimici@teads.tv>
2019-12-08 16:44:20 +0200

    day 5 - solved

--- days/advent-5.js
+++ days/advent-5.js
@@ -1,32 +1,81 @@
 const defaultMemory = [3,225,1,225,6,6,1100,1,238,225,104,0,1001,92,74,224,1001,224,-85,224,4,224,1002,223,8,223,101,1,224,224,1,223,224,223,1101,14,63,225,102,19,83,224,101,-760,224,224,4,224,102,8,223,223,101,2,224,224,1,224,223,223,1101,21,23,224,1001,224,-44,224,4,224,102,8,223,223,101,6,224,224,1,223,224,223,1102,40,16,225,1102,6,15,225,1101,84,11,225,1102,22,25,225,2,35,96,224,1001,224,-350,224,4,224,102,8,223,223,101,6,224,224,1,223,224,223,1101,56,43,225,101,11,192,224,1001,224,-37,224,4,224,102,8,223,223,1001,224,4,224,1,223,224,223,1002,122,61,224,1001,224,-2623,224,4,224,1002,223,8,223,101,7,224,224,1,223,224,223,1,195,87,224,1001,224,-12,224,4,224,1002,223,8,223,101,5,224,224,1,223,224,223,1101,75,26,225,1101,6,20,225,1102,26,60,224,101,-1560,224,224,4,224,102,8,223,223,101,3,224,224,1,223,224,223,4,223,99,0,0,0,677,0,0,0,0,0,0,0,0,0,0,0,1105,0,99999,1105,227,247,1105,1,99999,1005,227,99999,1005,0,256,1105,1,99999,1106,227,99999,1106,0,265,1105,1,99999,1006,0,99999,1006,227,274,1105,1,99999,1105,1,280,1105,1,99999,1,225,225,225,1101,294,0,0,105,1,0,1105,1,99999,1106,0,300,1105,1,99999,1,225,225,225,1101,314,0,0,106,0,0,1105,1,99999,108,677,226,224,102,2,223,223,1006,224,329,1001,223,1,223,1108,226,677,224,1002,223,2,223,1006,224,344,101,1,223,223,7,226,677,224,102,2,223,223,1006,224,359,1001,223,1,223,1007,226,677,224,1002,223,2,223,1006,224,374,1001,223,1,223,1108,677,226,224,102,2,223,223,1005,224,389,1001,223,1,223,107,226,226,224,102,2,223,223,1006,224,404,101,1,223,223,1107,226,226,224,1002,223,2,223,1005,224,419,1001,223,1,223,1007,677,677,224,102,2,223,223,1006,224,434,101,1,223,223,1107,226,677,224,1002,223,2,223,1006,224,449,101,1,223,223,107,677,677,224,102,2,223,223,1005,224,464,1001,223,1,223,1008,226,226,224,1002,223,2,223,1005,224,479,101,1,223,223,1007,226,226,224,102,2,223,223,1005,224,494,1001,223,1,223,8,677,226,224,1002,223,2,223,1005,224,509,1001,223,1,223,108,677,677,224,1002,223,2,223,1005,224,524,1001,223,1,223,1008,677,677,224,102,2,223,223,1006,224,539,1001,223,1,223,7,677,226,224,1002,223,2,223,1005,224,554,101,1,223,223,1108,226,226,224,1002,223,2,223,1005,224,569,101,1,223,223,107,677,226,224,102,2,223,223,1005,224,584,101,1,223,223,8,226,226,224,1002,223,2,223,1005,224,599,101,1,223,223,108,226,226,224,1002,223,2,223,1006,224,614,1001,223,1,223,7,226,226,224,102,2,223,223,1006,224,629,1001,223,1,223,1107,677,226,224,102,2,223,223,1005,224,644,101,1,223,223,8,226,677,224,102,2,223,223,1006,224,659,1001,223,1,223,1008,226,677,224,1002,223,2,223,1006,224,674,1001,223,1,223,4,223,99,226];
-const knownOptCodes = [1, 2, 3, 4, 99];
+const knownOptCodes = [1, 2, 3, 4, 5, 6, 7, 8, 99];
 
 function processOptCode(memory, index, optcode, input, paramModes) {
-    if (optcode === 99) {
-        return null;
-    } else if (optcode === 1) {
-        const fnSum = paramModes[0] === 0 ? memory[index + 1] : index + 1;
-        const snSum = paramModes[1] === 0 ? memory[index + 2] : index + 2;
-        const tSum = paramModes[2] === 0 ? memory[index + 3] : index + 3;
-        memory[tSum] = memory[fnSum] + memory[snSum];
-        return index + 4;
-    } else if (optcode === 2) {
-        const fnMul = paramModes[0] === 0 ? memory[index + 1] : index + 1;
-        const snMul = paramModes[1] === 0 ? memory[index + 2] : index + 2;
-        const tMul = paramModes[2] === 0 ? memory[index + 3] : index + 3;
-        memory[tMul] = memory[fnMul] * memory[snMul];
-        return index + 4;
-    } else if (optcode === 3) {
-        const sPosition = memory[index+1];
-        memory[sPosition] = input;
-        return index + 2;
-    } else if (optcode === 4) {
-        const wPosition = memory[index+1];
-        console.log('[OUT]:_> ', memory[wPosition]);
-        return index + 2;
-    } else {
-        console.log("Something went wrong");
-        return null;   
+    switch (optcode) {
+        case 1: {
+            const fnSum = paramModes[0] === 0 ? memory[index + 1] : index + 1;
+            const snSum = paramModes[1] === 0 ? memory[index + 2] : index + 2;
+            const tSum = paramModes[2] === 0 ? memory[index + 3] : index + 3;
+            memory[tSum] = memory[fnSum] + memory[snSum];
+            return index + 4;
+        }
+        case 2: {
+            const fnMul = paramModes[0] === 0 ? memory[index + 1] : index + 1;
+            const snMul = paramModes[1] === 0 ? memory[index + 2] : index + 2;
+            const tMul = paramModes[2] === 0 ? memory[index + 3] : index + 3;
+            memory[tMul] = memory[fnMul] * memory[snMul];
+            return index + 4;
+        }
+        case 3: {
+            const sPosition = paramModes[0] === 0 ? memory[index + 1] : index + 1;
+            memory[sPosition] = input;
+            return index + 2;
+        }
+        case 4: {
+            const wPosition = paramModes[0] === 0 ? memory[index + 1] : index + 1;
+            console.log('[OUT]:_> ', memory[wPosition]);
+            return index + 2;
+        }
+        case 5: {
+            const jumpTrueP1 = paramModes[0] === 0 ? memory[index + 1] : index + 1;
+            const jumpTrueP2 = paramModes[1] === 0 ? memory[index + 2] : index + 2;
+            if (memory[jumpTrueP1] !== 0) {
+                return memory[jumpTrueP2];
+            } else {
+                return index + 3;
+            }
+        }
+        case 6: {
+            const jumpFalseP1 = paramModes[0] === 0 ? memory[index + 1] : index + 1;
+            const jumpFalseP2 = paramModes[1] === 0 ? memory[index + 2] : index + 2;
+            if (memory[jumpFalseP1] === 0) {
+                return memory[jumpFalseP2];
+            } else {
+                return index + 3;
+            }
+        }
+        case 7: {
+            const lessThanP1 = paramModes[0] === 0 ? memory[index + 1] : index + 1;
+            const lessThanP2 = paramModes[1] === 0 ? memory[index + 2] : index + 2;
+            const lessThanP3 = paramModes[2] === 0 ? memory[index + 3] : index + 3;
+            if (memory[lessThanP1] < memory[lessThanP2]) {
+                memory[lessThanP3] = 1;
+                return index + 4;
+            } else {
+                memory[lessThanP3] = 0;
+                return index + 4;
+            }
+        }
+        case 8: {
+            const equalP1 = paramModes[0] === 0 ? memory[index + 1] : index + 1;
+            const equalP2 = paramModes[1] === 0 ? memory[index + 2] : index + 2;
+            const equalP3 = paramModes[2] === 0 ? memory[index + 3] : index + 3;
+            if (memory[equalP1] === memory[equalP2]) {
+                memory[equalP3] = 1;
+                return index + 4;
+            } else {
+                memory[equalP3] = 0;
+                return index + 4;
+            }
+        }
+        case 99: {
+            return null;
+        }
+        default: {
+            console.log("Something went wrong");
+            return null;
+        }
     }
 }
 
@@ -87,3 +136,4 @@ function intCodeComputer(source, input) {
 }
 
 intCodeComputer(defaultMemory, 1);
+intCodeComputer(defaultMemory, 5);
